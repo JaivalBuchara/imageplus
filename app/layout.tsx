043f420908d3356e01323aa0f1289abaa068f61e
JaivalBuchara <jaival.buchara2021@vitstudent.ac.in>
2024-07-03 17:03:10 +0530

    Modified layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Room from "./Room";
 
 export const metadata = {
-  title: "Figma Clone",
+  title: "ImagePlus+",
   description:
     "A minimalist Figma clone using fabric.js and Liveblocks for realtime collaboration",
 };
